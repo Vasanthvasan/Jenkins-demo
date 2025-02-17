5d622f0bb497a0972981847f3b2ed4de6be69085
Your New Name <your.email@example.com>
2025-02-18 01:27:08 +0530

    'changes'

--- src/main/java/com/Test/jenkins_Test/JenkinsTestApplication.java
+++ src/main/java/com/Test/jenkins_Test/JenkinsTestApplication.java
@@ -4,16 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @SpringBootApplication
 public class JenkinsTestApplication extends SpringBootServletInitializer {
